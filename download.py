4bc4b6b9eb44b9264539aaea75a675872d794bfe
Noreh AD <akinoreh@gmail.com>
2024-07-09 16:27:28 +0300

    TODOs

---
+++ download.py
@@ -0,0 +1,8 @@
+# TODO: get playlist.json response
+# TODO: select video
+# TODO: download video segments
+# TODO: merge video segments
+# TODO: select audio
+# TODO: download audio segments
+# TODO: merge audio segments
+# TODO: merge the video and the audio
