08f54ae8f17de282fe217b6070ea517b19bca67b
Noreh AD <akinoreh@gmail.com>
2024-07-31 15:31:00 +0300

    get playlist.json response

--- download.py
+++ download.py
@@ -1,4 +1,6 @@
 import json
+import sys
+import requests
 from helpers import input_adv, is_playlist_json_url, parse_playlist_url
 
 playlist_url = input_adv(
@@ -10,11 +12,21 @@ parsed_playlist_url = parse_playlist_url(playlist_url)
 
 print(json.dumps(parsed_playlist_url, indent=4))
 
+playlist_response = requests.get(playlist_url)
+
+if playlist_response.ok is False:
+    print("Request to the playlist URL failed. URL might be expired. Try a new one.")
+    input()
+    sys.exit()
+
+json_data = json.loads(playlist_response.text)
+
+print(json.dumps(json_data, indent=4))
+
 print("Moving on ...")
 input()
 
 
-# TODO: get playlist.json response
 # TODO: select video
 # TODO: download video segments
 # TODO: merge video segments
